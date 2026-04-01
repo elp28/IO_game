048b1f0cbd063b5509f5fb86f75ebd5430e10fc5
GuiDominoni <guidominonig@gmail.com>
2026-04-01 11:37:18 -0300

    movimentação melhorada, falta o ataque

--- IO_GAME/Assets/Guilherme/Scripts/PlayerMove.cs
+++ IO_GAME/Assets/Guilherme/Scripts/PlayerMove.cs
@@ -9,12 +9,14 @@ public class PlayerMove : MonoBehaviour
     Vector2 movement;
     bool isMoving;
     Animator anim;
+    SpriteRenderer sp;
+    bool isLeft;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
-
+        sp = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -24,14 +26,28 @@ public class PlayerMove : MonoBehaviour
         movement.y = joy.Vertical;
         if (movement != Vector2.zero)
         {
+            sp.flipX = false;
             float angle = Mathf.Atan2(movement.y, movement.x) * Mathf.Rad2Deg;
             rb.rotation = angle;
             isMoving = true;
+            if (movement.x < 0)
+            {
+                isLeft = true;
+                sp.flipY = true;
+            }
+            else
+            {
+                isLeft = false;
+                sp.flipY = false;
+            }
         }
         else
         {
             isMoving = false;
             rb.rotation = 0;
+            if(isLeft)
+                sp.flipX = true;
+            sp.flipY = false;
         }
         
         anim.SetBool("isSwimming", isMoving);

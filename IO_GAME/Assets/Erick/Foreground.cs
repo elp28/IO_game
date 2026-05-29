f829f5b9217b9c393d109d8397524a5c1303aaac
elp28 <elpbr2807@gmail.com>
2026-05-29 10:37:23 -0300

    foreground funcionando

--- IO_GAME/Assets/Erick/Foreground.cs
+++ IO_GAME/Assets/Erick/Foreground.cs
@@ -1,46 +1,47 @@
 using UnityEngine;
-using UnityEngine.Tilemaps;
 
 public class Foreground : MonoBehaviour
 {
     [Header("Configurações de Parallax")]
     public Transform cameraTransform;
-    public float parallaxFactor = -0.5f; 
+    [Range(-1f, 1f)]
+    public float parallaxFactor = 0.2f;
 
     [Header("Configurações de Visibilidade")]
     public Transform player;
     public float detectionRadius = 2.0f;
-    public float minAlpha = 0.3f; 
+    public float minAlpha = 0.3f;
     public float fadeSpeed = 5f;
 
-    private Tilemap tilemap;
-    private Vector3 lastCameraPosition;
+    private SpriteRenderer spriteRenderer;
+    private Vector3 startPos;
+    private Vector3 startCamPos;
 
     void Start()
     {
-        tilemap = GetComponent<Tilemap>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
         if (cameraTransform == null) cameraTransform = Camera.main.transform;
-        lastCameraPosition = cameraTransform.position;
+
+        startPos = transform.position;
+        startCamPos = cameraTransform.position;
     }
 
     void LateUpdate()
     {
-       
-        Vector3 deltaMovement = cameraTransform.position - lastCameraPosition;
-        transform.position += deltaMovement * parallaxFactor;
-        lastCameraPosition = cameraTransform.position;
+        Vector3 camDelta = cameraTransform.position - startCamPos;
+        transform.position = startPos + camDelta * parallaxFactor;
 
-        
         HandleAlphaFade();
     }
 
     void HandleAlphaFade()
     {
+        // Distância direto do player ao objeto
         float distance = Vector2.Distance(player.position, transform.position);
         float targetAlpha = (distance < detectionRadius) ? minAlpha : 1.0f;
 
-        Color curColor = tilemap.color;
-        float newAlpha = Mathf.MoveTowards(curColor.a, targetAlpha, fadeSpeed * Time.deltaTime);
-        tilemap.color = new Color(curColor.r, curColor.g, curColor.b, newAlpha);
+        Color curColor = spriteRenderer.color;
+        float newAlpha = Mathf.Lerp(curColor.a, targetAlpha, fadeSpeed * Time.deltaTime);
+        spriteRenderer.color = new Color(curColor.r, curColor.g, curColor.b, newAlpha);
     }
 }
